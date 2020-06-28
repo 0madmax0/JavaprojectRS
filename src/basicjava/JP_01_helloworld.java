92a15934732e4c2f6df7b22bb871efb653fc5182
0madmax0 <67330173+0madmax0@users.noreply.github.com>
2020-06-28 22:26:15 +0545

    Update JP_01_helloworld.java

--- src/basicjava/JP_01_helloworld.java
+++ src/basicjava/JP_01_helloworld.java
@@ -3,7 +3,7 @@ package basicjava;
 public class JP_01_helloworld {
 
 	public static void main(String[] args) {
-		//my first java program
+		//my first java program roshan1234
 		System.out.println("my name is roshan");
 
 	}
